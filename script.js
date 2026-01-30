235c6e95d7f7fc14af3a4539469c379e5d22c8ab
Op <113507103+Opiah8000LP@users.noreply.github.com>
2026-01-30 19:48:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const text = document.getElementById("love");
 
+function shakeText() {
+    text.classList.remove("shake");
+
+    // force reflow so animation restarts
+    void text.offsetHeight;
+
+    text.classList.add("shake");
+}
+
+document.addEventListener("click", shakeText);
